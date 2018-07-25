0a79267e7978e45512f292dc19fb836034e13fd2
vedddd <vedddd@gmail.com>
2018-07-25 18:54:58 +0300

    Добавил новую версию прошивки для AD7175-2

--- sketchbook/ADC_ad7175-2/ADC_ad7175_2.ino
+++ sketchbook/ADC_ad7175-2/ADC_ad7175_2.ino
@@ -1,13 +1,15 @@
 #include <SPI.h>
+#define A14 9
+#define A15 10
 
 void setup() {
   // put your setup code here, to run once:
    
-  Serial.begin(9600);
+  Serial.begin(9600); // default 9600 //max speed 4 000 000
   SPI.begin(); // initialize SPI, covering MOSI,MISO,SCK signals
   SPI.setBitOrder(MSBFIRST);  // data is clocked in MSB first
   SPI.setDataMode(SPI_MODE3);  // SCLK idle low (CPOL=0), MOSI read on rising edge (CPHI=0)
-  //SPI.setClockDivider(SPI_CLOCK_DIV8);  // system clock = 16 MHz, chip max = 1 MHz
+  //SPI.setClockDivider(SPI_CLOCK_DIV2);  // system clock = 16 MHz, chip max = 1 MHz
   pinMode(A15,OUTPUT); 
   pinMode(A14,INPUT);
   
@@ -15,8 +17,8 @@ void setup() {
   digitalWrite(A15, LOW);
   SPI.transfer(B00000010); // Interface mod register
   SPI.transfer(B00000000); // default
-  SPI.transfer(B00000001); // 16-bit mode
-  //SPI.transfer(B00000000); // 24-bit mode
+  //SPI.transfer(B00000001); // 16-bit mode
+  SPI.transfer(B00000000); // 24-bit mode
   digitalWrite(A15, HIGH);
   delay(5);
 
@@ -24,7 +26,7 @@ void setup() {
   digitalWrite(A15, LOW);
   SPI.transfer(B00100000); // Setup config 0 register
   SPI.transfer(B00010011); // default
-  SPI.transfer(B00000000); // Reference 2.5V (2.507 V)
+  //SPI.transfer(B00000000); // Reference 2.5V
   digitalWrite(A15, HIGH);
   delay(5);
   
@@ -55,25 +57,22 @@ void loop()
   
   //Serial.println(SPI.transfer(0xff),BIN);
   //Serial.println(SPI.transfer(0xff),BIN);
-  //Serial.println(SPI.transfer(0xff),DEC); // only 24 bit mode*/
+  //Serial.println(SPI.transfer(0xff),DEC); // only 24 bit mode
   //Serial.println();
   uint8_t byte_data_reg0 = SPI.transfer(0xff);
   uint8_t byte_data_reg1 = SPI.transfer(0xff);
-  //uint8_t byte_data_reg2 = SPI.transfer(0xff);
-  float usls = 256*float(byte_data_reg0)+float(byte_data_reg1)+1.0;
-  //float usls = 65536*float(byte_data_reg0)+256*float(byte_data_reg1)+float(byte_data_reg2)+1.0;
-  //float a2 = SPI.transfer(0xff)+1.0;
+  uint8_t byte_data_reg2 = SPI.transfer(0xff); // only 24 bit mode
   
-  //Serial.println(7.6507568e-5*(usls-32768),4); // 16 Bit Reference 2.5V (2.507 V)
-  //Serial.println(2.9861927e-7*(usls-8388608),6); // 24 Bit Reference 2.5V (2.507 V)
+  //float usls = 256*float(byte_data_reg0)+float(byte_data_reg1)+1.0; // 16 bit
+  float usls = 65536.0*float(byte_data_reg0)+256.0*float(byte_data_reg1)+float(byte_data_reg2)+1.0; // 24 bit
   
-  //Serial.println(0.000125305176781*(usls-32768),4); // 16 Bit Reference 4.096V (4.106 V)
-  //Serial.println(4.89473342896e-07*(usls-8388608),4); // 16 Bit Reference 4.096V (4.106 V)
+  //usls = 0.03384555*(usls-32768.0); // 16 Bit Vref - 2.5V
+  //usls = 1.31630905e-04*(usls-8388608.0); //  24 Bit Vref - 2.5V
   
-  Serial.println(0.000336678539*(usls-32768),4); // 16 Bit Reference 4.096V (4.106 V)
-  // 62465 -> +10V - 0.00033673435
-  // 32768 -> 0 V - 0.000336678539
-  // 3080 ->  -10V - 0.00033683643
+  //usls = 0.0003384555*(usls-32768.0); // 16 Bit Vref - 4.096V
+  usls = 1.31630905e-06*(usls-8388608.0); // 24 Bit Vref - 4.096V
+  
+  Serial.println(usls,5);
   //Serial.println(usls);
   
   digitalWrite(A15, HIGH);
